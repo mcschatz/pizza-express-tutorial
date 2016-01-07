d9912037237cd5494ea9cc53eadad4207b1a80eb
Mimi Schatz <mcschatz@gmail.com>
2016-01-07 11:54:33 -0700

    added request and server tests

--- test/server-test.js
+++ test/server-test.js
@@ -1,8 +1,48 @@
 const assert = require('assert');
+const request = require('request');
 const app = require('../server');
 
 describe('Server', () => {
+
+  before((done) => {
+    this.port = 9876;
+
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
+
   it('should exist', () => {
     assert(app);
   });
+
+  it('should have a body with the name of the application', (done) => {
+    var title = app.locals.title;
+
+    this.request.get('/', (error, response) => {
+      if (error) { done(error); }
+      assert(response.body.includes(title),
+             `"${response.body}" does not include "${title}".`);
+      done();
+    });
+  });
+
+  describe('GET /', () => {
+    it('should return a 200', (done) => {
+      this.request.get('/', (error, response) => {
+        assert.equal(response.statusCode, 200);
+        done();
+      });
+    });
+  });
+
 });
